d7b5f6d5bdea8b613c25c9a555a6cb9fdb0a72a4
Aliaksey-Iliukevich <aleks.ilykevich@gmail.com>
2024-02-05 06:13:31 +0300

    fix: task 2

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -2,6 +2,6 @@ let number1 = Math.floor(Math.random() * 100)
 
 if(number1 % 2 === 0){
     console.log(number1 + ': Это четное число');
-} else {
+} else if(number1 % 2 !== 0) {
     console.log(number1 + ': Это нечетное число');
 }
