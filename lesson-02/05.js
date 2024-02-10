84ac7d2f01c1d87daafd1bf6b2b22ed80f678521
Aliaksey-Iliukevich <aleks.ilykevich@gmail.com>
2024-02-10 15:44:51 +0300

    feat: lesson 2 task 5 done

---
+++ lesson-02/05.js
@@ -0,0 +1,18 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+function clone(obj) {
+    let clonedObj = {}
+
+    for (i in obj) clonedObj[i] = typeof obj[i] == "object" ? clone(obj[i]) : obj[i];
+
+    return clonedObj
+}
+
+let passport2_0 = clone(passport);
+passport2_0.name = "Ivan";
+
+console.log(passport);
+console.log(passport2_0);
